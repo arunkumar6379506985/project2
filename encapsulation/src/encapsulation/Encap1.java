190ac8b7639ad76e059879656c1c2db2f2181e1a
Arunkumar <arunkumar27199@gmail.com>
2022-04-23 10:14:54 +0530

    project 2

---
+++ encapsulation/src/encapsulation/Encap1.java
@@ -0,0 +1,20 @@
+package encapsulation;
+
+public class Encap1 {
+	protected String name;
+	private int a;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	
+
+}
---
+++ encapsulation/src/encapsulation/encap2.java
@@ -0,0 +1,14 @@
+package encapsulation;
+
+public class encap2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Encap1 e=new Encap1();
+
+e.setA(7);
+System.out.println(e.getA());
+	
+	}
+
+}
